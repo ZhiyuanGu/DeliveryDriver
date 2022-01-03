cf13d9a8b1abc9ed1dff7ea2b90ad0f15af19397
Zhiyuan Gu <42496605+ZhiyuanGu@users.noreply.github.com>
2022-01-02 23:13:54 -0500

    framerate independent & onTrigger/onCollision

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    [SerializeField] float steerSpeed = 0.3f;
-    [SerializeField] float moveSpeed = 0.01f;
+    [SerializeField] float steerSpeed = 100f;
+    [SerializeField] float moveSpeed = 5f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,8 +14,8 @@ public class Driver : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float moveAmount = Input.GetAxis("Vertical") * moveSpeed;
-        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
         transform.Translate(0, moveAmount, 0);
         transform.Rotate(0, 0, -steerAmount); // opposite direction
     }
